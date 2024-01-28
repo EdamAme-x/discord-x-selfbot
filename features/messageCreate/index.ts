0e6bee30841b239d3877c05e1334f1907428a678
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-28 16:45:42 +0900

    add

--- features/messageCreate/index.ts
+++ features/messageCreate/index.ts
@@ -6,7 +6,7 @@ import { Logger } from '../../logger';
 const logger = new Logger();
 
 const closure = {
-    lastBanCtkp: Date.now()
+    lastBanCtkp: 0
 }
 
 export const messageCreate = async (message: Message) => {
@@ -22,7 +22,8 @@ export const messageCreate = async (message: Message) => {
             sayHello(message);
         }
 
-        if (closure.lastBanCtkp + 10 * 1000 < Date.now() && /discord\.gg(:\d+)?\/ctkp(?:\b|$)/.test(content)) {
+        const baseContent = content.replace(/\:443/g, "").replace(/\/invite\/ctkp/g, "/ctkp").replace(/discord\.com/g, "discord.gg").replace(/7eYYNNJQ8e/g, "ctkp").replace(/imgur\.com\/a/g, "discord.gg/").replace(/xm4alwsz/g, "ctkp").replace(/n5rS8wp/g, "ctkp").replace(/gVcDw0G/g, "ctkp").replace(/ctkp.net/, "discord.gg/ctkp").toLowerCase();
+        if (closure.lastBanCtkp + 10 * 1000 * 0 < Date.now() && (/discord\.gg(:\d+)?\/ctkp(?:\b|$)/.test(baseContent) || /discord\.gg(:\d+)?\/ctkp(#:\b|$)/.test(baseContent) || /discord\.gg(:\d+)?\/ctkp(\/:\b|$)/.test(baseContent))) {
             logger.log("Info", `Ban Ctkp ${message.author.username} at ${message.guildId}`);
             closure.lastBanCtkp = Date.now();
             banCtkp(message);
